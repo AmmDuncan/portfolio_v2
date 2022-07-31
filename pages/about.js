8c7d4200f93dfea5fc48df7de0d6553c1115a0c9
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-07-31 19:14:10 +0000

    SEO: add twitter and open graph meta tags

--- pages/about.js
+++ pages/about.js
@@ -12,6 +12,26 @@ export default function AboutMe() {
           name="description"
           content="Hi my name is Ammiel, I'm a Frontend Web Developer with some practical knowledge in Backend Development."
         />
+
+        <meta property="og:url" content="https://www.ammielyawson.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="About | Ammiel Yawson" />
+        <meta
+          property="og:description"
+          content="Hi my name is Ammiel, I'm a Frontend Web Developer with some practical knowledge in Backend Development."
+        />
+        <meta property="og:image" content="https://ammielyawson.com/assets/images/SEO-Image.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ammielyawson.com" />
+        <meta property="twitter:url" content="https://www.ammielyawson.com/" />
+        <meta name="twitter:title" content="About | Ammiel Yawson" />
+        <meta
+          name="twitter:description"
+          content="Hi my name is Ammiel, I'm a Frontend Web Developer with some practical knowledge in Backend Development."
+        />
+        <meta name="twitter:image" content="https://ammielyawson.com/assets/images/SEO-Image.png" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
