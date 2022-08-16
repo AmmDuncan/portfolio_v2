46b7976c7fab82e1ddb9997163e0105d7a5aa1d9
ammduncan <ammielgyanyawson@gmail.com>
2022-08-16 17:41:23 +0000

    feat: navbar-> added route to old version

--- components/layout/Navbar/Navbar.helpers.js
+++ components/layout/Navbar/Navbar.helpers.js
@@ -1,8 +1,9 @@
 const navlist = [
-  {to: '/', display: "Home"},
-  {to: '/projects', display: "Projects"},
-  {to: '/about', display: "About Me"},
-  {to: 'https://blog.ammielyawson.com/', display: 'Blog', external: true, }
-]
+  { to: "/", display: "Home" },
+  { to: "/projects", display: "Projects" },
+  { to: "/about", display: "About Me" },
+  { to: "https://blog.ammielyawson.com/", display: "Blog", external: true },
+  { to: "https://v1.ammielyawson.com/", display: "V-one", external: true },
+];
 
-export {navlist}
+export { navlist };
