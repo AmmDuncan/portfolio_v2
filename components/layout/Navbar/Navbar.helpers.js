c5528a8016b958f0cac4174d2372c75bf22c6d9a
ammduncan <ammielgyanyawson@gmail.com>
2022-10-12 09:12:26 +0000

    add connect four

--- components/layout/Navbar/Navbar.helpers.js
+++ components/layout/Navbar/Navbar.helpers.js
@@ -3,7 +3,7 @@ const navlist = [
   { to: "/projects", display: "Projects" },
   { to: "/about", display: "About Me" },
   { to: "https://blog.ammielyawson.com/", display: "Blog", external: true },
-  { to: "https://v1.ammielyawson.com/", display: "V-one", external: true },
+  // { to: "https://v1.ammielyawson.com/", display: "V-one", external: true },
 ];
 
 export { navlist };
--- data/projects.js
+++ data/projects.js
@@ -5,14 +5,14 @@ export const projects = [
     description:
       "Public data bank that uses various visualization methods to simplify data for easy digestion by users.",
     tools: ["Vue", "Vuex", "Chart.js", "Storyblok", "Sass"],
-    link: "https://kwerty.io",
+    link: "https://kwerty.io"
   },
   {
     name: "HRMs by Enyata",
     imgSrc: "/assets/projects/hrms.png",
     description:
       "Software to manage Human Resource in a company from Hiring, to Assessments, to Employee managament.",
-    tools: ["React", "Chakra-ui", "Emotion", "React-Hook-Forms"],
+    tools: ["React", "Chakra-ui", "Emotion", "React-Hook-Forms"]
   },
   // { name: "Pomout", imgSrc: "/assets/projects/pomout.png" },
   {
@@ -20,14 +20,21 @@ export const projects = [
     imgSrc: "/assets/projects/devindets.png",
     description: "Marketing website for a Digital Agency.",
     tools: ["React", "Next.js", "Sass", "Figma"],
-    link: "https://dev-in-details.vercel.app/",
+    link: "https://dev-in-details.vercel.app/"
+  },
+  {
+    name: "Connect Four",
+    imgSrc: "/assets/projects/connect-four.png",
+    description: "My solution to the connect four game on FrontendMentor.io",
+    tools: ["Vue", "Pinia", "Vue-Router"],
+    link: "https://connect-four.ammielyawson.com/"
   },
   {
     name: "Portfolio",
     imgSrc: "/assets/projects/portfolio.png",
     description: "The site you're currently scrolling.",
-    tools: ["React", "Next.js", "Figma", "styled-components"],
-  },
+    tools: ["React", "Next.js", "Figma", "styled-components"]
+  }
 ];
 
 export const sideProjects = [
@@ -37,7 +44,7 @@ export const sideProjects = [
     description:
       "Productivity web application to help you work in short bursts to avoid burning out.",
     tools: ["React", "Styled-components", "Figma"],
-    link: "https://pomout.vercel.app",
+    link: "https://pomout.vercel.app"
   },
   {
     name: "Snake Game",
@@ -45,7 +52,7 @@ export const sideProjects = [
     description:
       "My version of retro snake game. Use arrow keys to move the snake about. On mobile, press toggle controls button to show on-screen controls.",
     tools: ["JavaScript", "HTML5", "CSS3"],
-    link: "https://snake.ammielyawson.com",
+    link: "https://snake.ammielyawson.com"
   },
   // { name: "Pomout", imgSrc: "/assets/projects/pomout.png" },
   {
@@ -53,20 +60,20 @@ export const sideProjects = [
     imgSrc: "/assets/projects/rock-paper-scissors.png",
     description: "Rock paper scissors challenge from FrontendMentor.io.",
     tools: ["Vue", "Sass"],
-    link: "https://myrockpaperscissors.vercel.app",
+    link: "https://myrockpaperscissors.vercel.app"
   },
   {
     name: "Movie Search App",
     imgSrc: "/assets/projects/movie-app.png",
     description: "Designed and developed to teach a class on single page apps with Vuejs",
     tools: ["Vue", "Vue Router", "Vuex", "Figma", "CSS"],
-    link: "https://movies.ammielyawson.com",
+    link: "https://movies.ammielyawson.com"
   },
   {
     name: "Countries App",
     imgSrc: "/assets/projects/countries-app.png",
     description: "Country search app challenge from FrontendMentor.io.",
     tools: ["Vue", "Vue Router", "Vuex", "CSS"],
-    link: "https://countries.ammielyawson.com",
-  },
+    link: "https://countries.ammielyawson.com"
+  }
 ];
